b45b0b7f6743a3f1cf9bad8f6b8115924ae19a12
sanishkharade <sanishkharade@gmail.com>
2022-04-07 14:09:17 -0600

    Updated the UART patch

--- modify.sh
+++ modify.sh
@@ -4,10 +4,15 @@ echo "Entering directory buildroot"
 cd buildroot/
 #echo "Rebuilding ldd package"
 #make ldd-rebuild
-echo "Rebuilding aesd-assignments package"
-make aesd-assignments-rebuild
-echo "Rebuilding aesd-char-device package"
-make aesd-char-device-rebuild
+#echo "Rebuilding aesd-assignments package"
+#make aesd-assignments-rebuild
+#echo "Rebuilding aesd-char-device package"
+#make aesd-char-device-rebuild
+
+
+echo "Rebuilding aesd-uart-driver package"
+make aesd-uart-driver-rebuild
+
 echo "Exiting directory buildroot"
 cd ..
 echo "Running build.sh"
